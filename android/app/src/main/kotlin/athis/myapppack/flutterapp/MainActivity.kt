66524cd787fe43752a38702ba3b8f969237f08a2
Abhishek <abhishekchauhan2277@gmail.com>
2020-06-07 18:40:37 +0530

    intial commit

---
+++ android/app/src/main/kotlin/athis/myapppack/flutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package athis.myapppack.flutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+// im poor app
+//void main() => runApp(MaterialApp(
+//      home: Scaffold(appBar: AppBar(title: Text("I'm Poor"),backgroundColor:
+//      Colors.teal[500],centerTitle: true,),
+//      body: Center(child: Image(image: AssetImage("images/ic.png"),),),
+//      backgroundColor: Colors.grey[500],),
+//    ));
+
+// im rich app
+//void main() => runApp(
+//      MaterialApp(
+//        home: (
+//        Scaffold(appBar: AppBar(title: Text("Hello F") ,backgroundColor: Colors.black45, centerTitle: true,),
+//        backgroundColor: Colors.blueGrey[800],
+//          body:
+//          Center(child: Image(image:AssetImage('images/diamond.png')
+//            ,),),
+//          bottomSheet: (new Text("Im Rich")),
+//        )
+//        ),
+//      ),
+//    );
+
+
+// mi card app
+ void main()=> runApp(MaterialApp(home: Scaffold(
+   appBar: AppBar(title: Text("Id App"),
+     centerTitle: true,backgroundColor: Colors.orange[500],),
+ backgroundColor: Colors.grey[700],)
+   ,));
